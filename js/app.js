4701bc67496026a6ce9df6570762d11b7b61367c
Renato Nascimento <renatocamisa3000@gmail.com>
2023-10-29 10:19:55 -0300

    versão atualizada

--- js/app.js
+++ js/app.js
@@ -1,60 +1,67 @@
+//variáveis.
 let currentInput = document.querySelector('.currentInput');
 let answerScreen = document.querySelector('.answerScreen');
 let buttons = document.querySelectorAll('button');
 let erasebtn = document.querySelector('#erase');
 let clearbtn = document.querySelector('#clear');
 let evaluate = document.querySelector('#evaluate');
+let porcent = document.querySelector('#porce');
 
+// Visor da calculadora.
+let realTimeScreenValue = [];
 
-
-// Visor da calculadora
-let realTimeScreenValue = []
-
-// Limpar
+// Limpar.
 clearbtn.addEventListener("click", () => {
-
     realTimeScreenValue = [''];
-    answerScreen.innerHTML = 0;
-    currentInput.className = 'currentInput'
-    answerScreen.className = 'answerScreen';
-    answerScreen.style.color = " rgba(150, 150, 150, 0.87)";
-})
+    updateScreen();
+});
 
-// Função anexada a todos os botões
-buttons.forEach((btn) => {
+// Função para atualizar os visores.
+function updateScreen() {
+    // Atualiza o visor principal (currentInput) com o conteúdo do array realTimeScreenValue.
+    currentInput.innerHTML = realTimeScreenValue.join('');
 
-
-    btn.addEventListener("click", () => {
-        // Se o botão clicado não é o botão de apagar
-        if (!btn.id.match('erase')) {
-            // Mostrar o valor do botão pressionado
-            realTimeScreenValue.push(btn.value)
-            currentInput.innerHTML = realTimeScreenValue.join('');
-
-            // Executar e mostrar a resposta em tempo real
-            if (btn.classList.contains('num_btn')) {
-
-                answerScreen.innerHTML = eval(realTimeScreenValue.join(''));
-
-            }
-
-        }
-
-        if (btn.id.match('erase')){
-            realTimeScreenValue.pop();
-            currentInput.innerHTML = realTimeScreenValue.join('');
+    // Verifica se o visor está vazio ou contém apenas uma string vazia.
+    if (realTimeScreenValue.length === 1 && realTimeScreenValue[0] === '') {
+        // Se estiver vazio, exibe '0' no visor de resposta (answerScreen).
+        answerScreen.innerHTML = '0';
+    } else {
+        try {
+            // Tenta avaliar a expressão no visor usando a função eval e exibe o resultado em answerScreen.
             answerScreen.innerHTML = eval(realTimeScreenValue.join(''));
+        } catch (error) {
+            // Se houver um erro ao avaliar a expressão, exibe uma string vazia no visor de resposta.
+            answerScreen.innerHTML = '';
         }
+    }
+}
 
-        if (btn.id.match('evaluate')){
-            currentInput.className = 'answerScreen';
-            answerScreen.className = 'currentInput';
-            answerScreen.style.color = "white";
+
+// Função anexada a todos os botões.
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (btn.id === 'clear') {
+            // Se o botão "Limpar" for clicado, redefine o valor do visor para uma string vazia.
+            realTimeScreenValue = [''];
+        } else if (btn.id === 'erase') {
+            // Se o botão "Apagar" for clicado, remove o último elemento do valor do visor.
+            realTimeScreenValue.pop();
+        } else if (btn.id === 'evaluate') {
+            // Se o botão "Avaliar" for clicado, defina o valor do visor como o resultado da expressão.
+            realTimeScreenValue = [answerScreen.innerHTML];
+        } else if (btn.id === 'porce') {
+            // Se o botão de porcentagem for clicado
+            // Obter o valor atual na tela e converter para um número, ou 0 se não for um número válido.
+            let currentValue = parseFloat(realTimeScreenValue.join('')) || 0;
+            // Calcular a porcentagem dividindo o valor atual por 100.
+            let percentage = currentValue / 100;
+            // Atualizar o valor no visor com a porcentagem calculada, convertendo-a de volta para uma string.
+            realTimeScreenValue = [percentage.toString()];
+        } else {
+            // Se qualquer outro botão for clicado, adicione o valor do botão ao valor do visor.
+            realTimeScreenValue.push(btn.value);
         }
-
-        if (typeof eval(realTimeScreenValue.join('')) == 'undefined'){
-           answerScreen.innerHTML = 0; 
-        }
-
-    })
-})
+        // Atualizar a interface do usuário com base no valor do visor após as operações.
+        updateScreen();
+    });
+});
